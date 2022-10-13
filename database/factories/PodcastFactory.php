0820c01d3795a74fe8aa4f4c76add862d2cf91bb
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-10-13 09:51:11 +0200

    Add user to podcast factory

--- database/factories/PodcastFactory.php
+++ database/factories/PodcastFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Enums\PodcastStatus;
 use App\Models\Podcast;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
@@ -14,6 +15,7 @@ class PodcastFactory extends Factory
     public function definition(): array
     {
         return [
+            'user_id' => User::factory(),
             'title' => $this->faker->word(),
             'description' => $this->faker->text(),
             'status' => PodcastStatus::DRAFT->value,
